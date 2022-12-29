8ca44896dbf0b798dfafd33ca4a9c9706d4271fc
Anna <goryachevaanny@gmail.com>
2022-12-29 22:11:15 +0400

    Easy level

--- app/src/main/java/com/goriacheva/calculator/MainActivity.java
+++ app/src/main/java/com/goriacheva/calculator/MainActivity.java
@@ -15,23 +15,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView textViewExample = findViewById(R.id.textViewExample);
         EditText editTextAnswer = findViewById(R.id.editTextAnswer);
         Button buttonAnswer = findViewById(R.id.buttonAnswer);
         TextView textViewCorrectAnswer = findViewById(R.id.textViewCorrectAnswer);
         TextView textViewIncorrectAnswer = findViewById(R.id.textViewIncorrectAnswer);
 
-buttonAnswer.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View view) {
-        String text = editTextAnswer.getText().toString();
-        if (text.equals("15")) {
-            textViewCorrectAnswer.setVisibility(View.VISIBLE);
-            textViewIncorrectAnswer.setVisibility(View.GONE);
-        } else {
-            textViewCorrectAnswer.setVisibility(View.GONE);
-            textViewIncorrectAnswer.setVisibility(View.VISIBLE);
-        }
-    }
-});
+        textViewExample.setText("10 + 20 = ?");
+
+        buttonAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = editTextAnswer.getText().toString();
+                if (text.equals("30")) {
+                    textViewCorrectAnswer.setVisibility(View.VISIBLE);
+                    textViewIncorrectAnswer.setVisibility(View.GONE);
+                } else {
+                    textViewCorrectAnswer.setVisibility(View.GONE);
+                    textViewIncorrectAnswer.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 }
